8fdb896319051241ddf3ad37907ecb8e97c8ce58
key00314 <key00314@tabgen8-1.acs.census.gov>
2022-09-09 10:11:02 -0400

    updating index column

--- Python/example1.py
+++ Python/example1.py
@@ -3,13 +3,13 @@ import pandas as pd
 
 
 def get_data(tblid, year, dataset, state):
-    #define urls for data and geography
+    #define urls for data and geography, update for 2021 1yr location
     data_url = f"https://www2.census.gov/programs-surveys/acs/summary_file/{year}/prototype/{dataset}YRData/acsdt{dataset}y{year}-{tblid}.dat"
     geo_url = f"https://www2.census.gov/programs-surveys/acs/summary_file/{year}/prototype/Geos{year}{dataset}YR.csv"
 
     #read data into dataframe
     data = pd.read_csv(data_url, sep='|', index_col="GEO_ID")
-    geos = pd.read_csv(geo_url, sep='|', index_col="DADSID")
+    geos = pd.read_csv(geo_url, sep='|', index_col="GEO_ID")
 
     #add geo file names and search for state
     data = data.join(geos[["NAME", "STUSAB"]])
